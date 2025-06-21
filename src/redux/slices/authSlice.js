7f6a166fe1442bd0901cbf59a7dcd76fcea87e96
Abdullah Iqbal <abdullah.iqbal1505@gmail.com>
2025-06-21 22:31:07 +0500

    small updates.

--- src/redux/slices/authSlice.js
+++ src/redux/slices/authSlice.js
@@ -1,6 +1,6 @@
 import { createSlice, createAsyncThunk } from '@reduxjs/toolkit';
 import { authAPI } from '@/lib/api';
-import jwt_decode from 'jwt-decode';
+import { jwtDecode } from 'jwt-decode';
 import axios from 'axios';
 
 // Async thunks
